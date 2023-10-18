56490db98dc594277828b4fc59adfc5adfd99d3e
Karlmathuthu <karlmathuthu11@gmail.com>
2023-10-18 12:16:41 +0200

    added Memoji class

---
+++ lib/utils/memojis.dart
@@ -0,0 +1,15 @@
+class Memojis {
+  // Man Memojis
+  static const man_1 = "assets/memoji/man_1.svg";
+  static const man_2 = "assets/memoji/man_2.svg";
+  static const man_3 = "assets/memoji/man_3.svg";
+  static const man_4 = "assets/memoji/man_4.svg";
+  static const man_5 = "assets/memoji/man_5.svg";
+
+  // Girl Memojis
+  static const girl_1 = "assets/memoji/girl_1.svg";
+  static const girl_2 = "assets/memoji/girl_2.svg";
+  static const girl_3 = "assets/memoji/girl_3.svg";
+  static const girl_4 = "assets/memoji/girl_4.svg";
+  static const girl_5 = "assets/memoji/girl_5.svg";
+}
