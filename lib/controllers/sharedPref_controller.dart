659b923ba6408679406a25b22be6966d3c577df5
Karlmathuthu <karlmathuthu11@gmail.com>
2023-10-18 12:13:34 +0200

    added memojis

---
+++ lib/controllers/sharedPref_controller.dart
@@ -0,0 +1,12 @@
+import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedPrefController extends GetxController {
+  //Save profile picture svg code
+  Future<void> saveSvgCodeProfilePicture({required String photoUrl}) async {
+    final SharedPreferences sharedPreferences =
+        await SharedPreferences.getInstance();
+
+    sharedPreferences.setString("photoUrl", photoUrl);
+  }
+}
