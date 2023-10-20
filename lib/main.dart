7b1c3690f35e9042b6b7c05efd7a4623a2a27a9a
Karlmathuthu <karlmathuthu11@gmail.com>
2023-10-20 11:29:35 +0200

    initialized notifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,44 @@
+import 'dart:convert';
+
 import 'package:chat_mate_messanger/routes/route_class.dart';
 import 'package:chat_mate_messanger/utils/constants.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/get_navigation.dart';
 
+import 'controllers/notifications_controller.dart';
 import 'firebase_options.dart';
+
+//Listen to background Notifications
+Future _firebaseBackgroundNotification(RemoteMessage message) async {
+  if (message.notification != null) {}
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
+  );
+  //Initialize Notifications
+  await NotificationsController.initNotification();
+  await NotificationsController.localNotiInit();
+  //Initialize background notifcations.
+  FirebaseMessaging.onBackgroundMessage(_firebaseBackgroundNotification);
+
+  //Handle foreground notifications
+  FirebaseMessaging.onMessage.listen(
+    (RemoteMessage message) {
+      String notificationPayLoad = jsonEncode(message.data);
+
+      if (message.notification != null) {
+        NotificationsController.showSimpleNotification(
+          title: message.notification!.title!,
+          body: message.notification!.body!,
+          payload: notificationPayLoad,
+        );
+      }
+    },
   );
   runApp(const MainApp());
 }
