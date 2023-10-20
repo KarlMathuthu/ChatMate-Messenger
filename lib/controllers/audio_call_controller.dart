7ed199ba90aa0d583c9036b29f0e1a8c8245e257
Karlmathuthu <karlmathuthu11@gmail.com>
2023-10-20 20:44:59 +0200

    working on webrtc

--- lib/controllers/audio_call_controller.dart
+++ lib/controllers/audio_call_controller.dart
@@ -2,75 +2,81 @@ import 'package:flutter_webrtc/flutter_webrtc.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AudioCallHandler {
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  RTCPeerConnection? peerConnection;
+  MediaStream? localStream;
+  MediaStream? remoteStream;
 
   // Initialize the WebRTC peer connection
-  RTCPeerConnection? _peerConnection;
+  Future<void> initializePeerConnection() async {
+    final Map<String, dynamic> configuration = {
+      'iceServers': [
+        {
+          'urls': [
+            'stun:stun1.l.google.com:19302',
+            'stun:stun2.l.google.com:19302'
+          ]
+        }
+      ]
+    };
 
-  // Initialize a local stream
-  MediaStream? _localStream;
+    peerConnection = await createPeerConnection(configuration, {});
+  }
 
-  // Initialize the remote stream
-  MediaStream? _remoteStream;
+  // Initialize a local stream (microphone audio)
+  Future<void> initializeLocalStream() async {
+    localStream = await navigator.mediaDevices.getUserMedia(
+      {
+        'video': false,
+        'audio': true,
+      },
+    );
+    peerConnection!.addStream(localStream!);
+  }
 
   // Initialize a collection in Firestore to store call information
-  final CollectionReference _callCollection = FirebaseFirestore.instance.collection('calls');
+  final CollectionReference callCollection =
+      FirebaseFirestore.instance.collection('calls');
 
   // Function to initiate a call
-  Future<void> makeCall(String callerUid, String receiverUid) async {
+  Future<void> makeCall(String toUserId) async {
     // Create an offer
-    _peerConnection = await createPeerConnection();
-
-    // Add a local stream (your microphone audio)
-    _localStream = await createStream();
-    _peerConnection?.addStream(_localStream!);
-
-    // Create an offer
-    RTCSessionDescription offer = await _peerConnection?.createOffer();
-
-    // Set the local description
-    await _peerConnection?.setLocalDescription(offer);
+    final offer = await peerConnection!.createOffer({'offerToReceiveAudio': 1});
+    await peerConnection!.setLocalDescription(offer);
 
     // Store the offer in Firestore
-    await _callCollection.doc(callerUid).set({'offer': offer.sdp});
+    await callCollection.doc(toUserId).set({
+      'offer': offer.toMap(),
+    });
 
     // Listen for the answer in Firestore
-    _callCollection.doc(receiverUid).snapshots().listen((event) async {
-      final data = event.data() as Map<String, dynamic>;
-      final answer = RTCSessionDescription(data['answer'], 'answer');
-      await _peerConnection?.setRemoteDescription(answer);
+    callCollection.doc(toUserId).snapshots().listen((snapshot) async {
+      if (snapshot.exists) {
+        final answer = snapshot['answer'];
+        await peerConnection!
+            .setRemoteDescription(RTCSessionDescription(answer, 'answer'));
+      }
     });
   }
 
   // Function to answer an incoming call
-  Future<void> answerCall(String callerUid, String receiverUid) async {
-    // Create an answer
-    _peerConnection = await createPeerConnection();
-
-    // Add a local stream (your microphone audio)
-    _localStream = await createStream();
-    _peerConnection?.addStream(_localStream!);
-
-    // Listen for the offer in Firestore
-    _callCollection.doc(callerUid).snapshots().listen((event) async {
-      final data = event.data() as Map<String, dynamic>;
-      final offer = RTCSessionDescription(data['offer'], 'offer');
-      await _peerConnection?.setRemoteDescription(offer);
+  Future<void> answerCall(String fromUserId) async {
+    final offer = await callCollection.doc(fromUserId).get();
+    if (offer.exists) {
+      final remoteOffer = RTCSessionDescription(offer['offer'], 'offer');
+      await peerConnection!.setRemoteDescription(remoteOffer);
 
       // Create an answer
-      RTCSessionDescription answer = await _peerConnection?.createAnswer();
-
-      // Set the local description
-      await _peerConnection?.setLocalDescription(answer);
+      final answer = await peerConnection!.createAnswer({});
+      await peerConnection!.setLocalDescription(answer);
 
       // Store the answer in Firestore
-      await _callCollection.doc(receiverUid).set({'answer': answer.sdp});
-    });
+      await callCollection.doc(fromUserId).set({'answer': answer.toMap()});
+    }
   }
 
   // Function to end a call
   Future<void> endCall() async {
-    _peerConnection?.close();
-    _localStream?.getTracks().forEach((track) => track.stop());
+    await peerConnection?.close();
+    localStream?.getTracks().forEach((track) => track.stop());
   }
 }
