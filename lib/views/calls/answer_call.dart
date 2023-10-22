f9a3085a0aa17a5c86d1ba2b4954e8420d4cc4cf
Karlmathuthu <karlmathuthu11@gmail.com>
2023-10-22 10:37:54 +0200

    working on webrtc

---
+++ lib/views/calls/answer_call.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AnswerCallPage extends StatefulWidget {
+  const AnswerCallPage({super.key});
+
+  @override
+  State<AnswerCallPage> createState() => _AnswerCallPageState();
+}
+
+class _AnswerCallPageState extends State<AnswerCallPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
