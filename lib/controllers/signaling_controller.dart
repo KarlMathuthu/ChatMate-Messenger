176e7451ef5cdb6b86f1fbb1d9a5192b9271d84f
Karlmathuthu <karlmathuthu11@gmail.com>
2023-10-22 10:39:54 +0200

    working on webrtc

--- lib/controllers/signaling_controller.dart
+++ lib/controllers/signaling_controller.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
 
+import 'package:chat_mate_messanger/views/calls/answer_call.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_webrtc/flutter_webrtc.dart';
 
@@ -139,8 +141,13 @@ class Signaling {
       if (querySnapshot.docChanges.isNotEmpty) {
         for (var change in querySnapshot.docChanges) {
           if (change.type == DocumentChangeType.added) {
-            print("Something else added in calls");
-           Navigator.push(context, route)
+            print("You have an incoming call");
+            Navigator.push(
+              context,
+              CupertinoPageRoute(
+                builder: (context) => AnswerCallPage(),
+              ),
+            );
           }
         }
       }
