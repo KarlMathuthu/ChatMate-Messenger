002a6c9ad85f387a69cbdccbae3b29f0a7f05ff9
Karlmathuthu <karlmathuthu11@gmail.com>
2023-10-26 11:14:43 +0200

    added some images

--- lib/utils/images.dart
+++ lib/utils/images.dart
@@ -1,6 +1,8 @@
 class Images {
-  static const splashImage = "";
-  static const letsYouInImage = "";
+  static const splashImage = "assets/images/splash.png";
+  static const letsYouInImage = "assets/images/letsyouin.png";
+  static const appLogoTransparent = "assets/images/splash.png";
+  static const splashImage2 = "assets/images/splash_2.png";
   static const onboardImageOne = "";
   static const onboardImageTwo = "";
   static const onboardImageThree = "";
