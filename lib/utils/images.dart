edb25bb3dd83bdf86aefb9d17c0685c60a20b99a
Karlmathuthu <karlmathuthu11@gmail.com>
2023-10-26 11:10:25 +0200

    add a class to make it easir to get images

---
+++ lib/utils/images.dart
@@ -0,0 +1,7 @@
+class Images {
+  static const splashImage = "";
+  static const letsYouInImage = "";
+  static const onboardImageOne = "";
+  static const onboardImageTwo = "";
+  static const onboardImageThree = "";
+}
