0b522d55617b5f591794669ab1eea30d90633fd9
Karlmathuthu <karlmathuthu11@gmail.com>
2023-10-28 15:28:09 +0200

    added some functions

--- lib/controllers/sharedPref_controller.dart
+++ lib/controllers/sharedPref_controller.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -9,5 +11,31 @@ class SharedPrefController extends GetxController {
 
     sharedPreferences.setString("photoUrl", photoUrl);
   }
-  //Save name and bio
+
+  // Convert List<Map<String, dynamic>> to JSON string
+  String convertToJson(List<Map<String, dynamic>> data) {
+    return json.encode(data);
+  }
+
+// Convert JSON string to List<Map<String, dynamic>>
+  List<Map<String, dynamic>> convertFromJson(String jsonString) {
+    return List<Map<String, dynamic>>.from(json.decode(jsonString));
+  }
+
+// Storing the data in shared preferences
+  void saveChatsData(List<Map<String, dynamic>> data) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String jsonData = convertToJson(data);
+    await prefs.setString('chats', jsonData);
+  }
+
+// Retrieving the data from shared preferences
+  Future<List<Map<String, dynamic>>> retrieveChatsData() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? jsonData = prefs.getString('chats');
+    if (jsonData != null) {
+      return convertFromJson(jsonData);
+    }
+    return [];
+  }
 }
