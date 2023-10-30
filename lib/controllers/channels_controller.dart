606798c28605521c8e36eb5d27d0693fc654399d
Karlmathuthu <karlmathuthu11@gmail.com>
2023-10-30 13:30:14 +0200

    added channels controller

---
+++ lib/controllers/channels_controller.dart
@@ -0,0 +1,8 @@
+import 'package:get/get.dart';
+
+class ChannelsController extends GetxController {
+  //Join Channel.
+  //Create Channel.
+  //Like Post in channel.
+  //Turn on Noitifications for channel.
+}
