f300a994d2040502b7a2e0b6f4732cfc5f3478a9
Karlmathuthu <karlmathuthu11@gmail.com>
2023-10-31 16:14:02 +0200

    modified:   lib/controllers/call_controller.dart

--- lib/controllers/call_controller.dart
+++ lib/controllers/call_controller.dart
@@ -1,8 +1,8 @@
 import 'package:get/get.dart';
 
 class CallController extends GetxController {
-  //Create Call
-  //Join Call
+  //Create Call.
+  //Join Call.
   //Close call.
   //Add call history to all mates.
 }
