fe3778739b59096e5b0a52ba20a504c9bcf5774e
Karlmathuthu <karlmathuthu11@gmail.com>
2023-11-01 13:27:45 +0200

    added chiplist package

--- lib/views/channels/create_channel.dart
+++ lib/views/channels/create_channel.dart
@@ -5,6 +5,8 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import '../../widgets/chip_list.dart';
+
 class CreateChannelPage extends StatefulWidget {
   const CreateChannelPage({super.key});
 
@@ -13,7 +15,28 @@ class CreateChannelPage extends StatefulWidget {
 }
 
 class _CreateChannelPageState extends State<CreateChannelPage> {
-  List<String> topicsList = [];
+  List<String> topicsList = [
+    "Anime",
+    "Movies",
+    "Cartoons",
+    "Random Chat",
+    "Attack on Titan",
+    "Dating",
+    "School",
+    "Studies",
+    "Business",
+    "Crypto",
+    "Programming",
+    "Developers",
+    "Donations",
+    "Indian Movies",
+    "Bundocks",
+    "Random Mates",
+    "Movies",
+    "MSA",
+    "TikTok Links",
+  ];
+  int selectedTopic = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -54,51 +77,74 @@ class _CreateChannelPageState extends State<CreateChannelPage> {
         height: double.infinity,
         margin: const EdgeInsets.only(top: 0.8),
         padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            //Pick Picture & TextField.
-            Row(
-              children: [
-                CircleAvatar(
-                  radius: 30,
-                  backgroundColor: AppTheme.mainColor,
-                  child: Center(
-                    child: SvgPicture.asset(
-                      CustomIcons.camera,
-                      colorFilter: const ColorFilter.mode(
-                        Colors.white,
-                        BlendMode.srcIn,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              //Pick Picture & TextField.
+              Row(
+                children: [
+                  CircleAvatar(
+                    radius: 30,
+                    backgroundColor: AppTheme.mainColor,
+                    child: Center(
+                      child: SvgPicture.asset(
+                        CustomIcons.camera,
+                        colorFilter: const ColorFilter.mode(
+                          Colors.white,
+                          BlendMode.srcIn,
+                        ),
                       ),
                     ),
                   ),
-                ),
-                const SizedBox(width: 5),
-                Expanded(
-                  child: TextField(
-                    style: GoogleFonts.lato(fontSize: 15),
-                    decoration: const InputDecoration(
-                      hintText: "Channel name",
+                  const SizedBox(width: 5),
+                  Expanded(
+                    child: TextField(
+                      style: GoogleFonts.lato(fontSize: 15),
+                      decoration: const InputDecoration(
+                        hintText: "Channel name",
+                      ),
                     ),
                   ),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: SvgPicture.asset(CustomIcons.emoji),
-                ),
-              ],
-            ),
-            const SizedBox(height: 15),
-            //Topics
-            Text(
-              "Choose Channel Topics",
-              overflow: TextOverflow.ellipsis,
-              style: GoogleFonts.lato(
-                color: Colors.black,
-                fontSize: 13,
+                  IconButton(
+                    onPressed: () {},
+                    icon: SvgPicture.asset(CustomIcons.emoji),
+                  ),
+                ],
               ),
-            ),
-          ],
+              const SizedBox(height: 15),
+              //Topics
+              Text(
+                "Choose Channel Topic",
+                overflow: TextOverflow.ellipsis,
+                style: GoogleFonts.lato(
+                  color: Colors.black,
+                  fontSize: 13,
+                ),
+              ),
+              const SizedBox(height: 10),
+
+              ChipList(
+                listOfChipNames: topicsList,
+                activeBgColorList: const [AppTheme.mainColor],
+                inactiveBgColorList: const [Colors.white],
+                activeTextColorList: const [Colors.white],
+                inactiveTextColorList: const [AppTheme.mainColor],
+                listOfChipIndicesCurrentlySeclected: [selectedTopic],
+                activeBorderColorList: const [AppTheme.mainColor],
+                inactiveBorderColorList: const [AppTheme.mainColor],
+                shouldWrap: true,
+                style: GoogleFonts.lato(fontSize: 13),
+                supportsMultiSelect: false,
+                wrapAlignment: WrapAlignment.start,
+                borderRadiiList: const [20],
+                extraOnToggle: (index) {
+                  selectedTopic = index;
+                  setState(() {});
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
