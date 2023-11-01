dce6ee61bef24d2c546f56a9ca5805f82b57ae7c
Karlmathuthu <karlmathuthu11@gmail.com>
2023-11-01 10:23:32 +0200

    updated

--- lib/views/channels/create_channel.dart
+++ lib/views/channels/create_channel.dart
@@ -1,4 +1,7 @@
+import 'package:chat_mate_messanger/theme/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CreateChannelPage extends StatefulWidget {
   const CreateChannelPage({super.key});
@@ -10,6 +13,26 @@ class CreateChannelPage extends StatefulWidget {
 class _CreateChannelPageState extends State<CreateChannelPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: AppTheme.scaffoldBacgroundColor,
+      appBar: AppBar(
+        title: Text(
+          "Create Channel",
+          style: GoogleFonts.lato(
+            color: Colors.black,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        leading: IconButton(
+          onPressed: () {
+            Get.back();
+          },
+          icon: const Icon(
+            Icons.arrow_back,
+          ),
+        ),
+      ),
+    );
   }
 }
