686b760ae0f1eb41c876ee8e03bfa8e6d65c7b2c
Karlmathuthu <karlmathuthu11@gmail.com>
2023-11-04 14:47:48 +0200

    updated

--- lib/utils/avatars.dart
+++ lib/utils/avatars.dart
@@ -1,15 +1,11 @@
 class Avatars {
-  // Man Memojis
-  static const man_1 = "assets/memoji/man_1.svg";
-  static const man_2 = "assets/memoji/man_2.svg";
-  static const man_3 = "assets/memoji/man_3.svg";
-  static const man_4 = "assets/memoji/man_4.svg";
-  static const man_5 = "assets/memoji/man_5.svg";
+  // Boy Avatars
+  static const boy_1 = "assets/avatars/boy_1.png";
+  static const boy_2 = "assets/avatars/boy_2.png";
+  static const boy_3 = "assets/avatars/boy_3.png";
 
-  // Girl Memojis
-  static const girl_1 = "assets/memoji/girl_1.svg";
-  static const girl_2 = "assets/memoji/girl_2.svg";
-  static const girl_3 = "assets/memoji/girl_3.svg";
-  static const girl_4 = "assets/memoji/girl_4.svg";
-  static const girl_5 = "assets/memoji/girl_5.svg";
+  // Girl Avatars
+  static const girl_1 = "assets/avatars/girl_1.png";
+  static const girl_2 = "assets/avatars/girl_2.png";
+  static const girl_3 = "assets/avatars/girl_3.png";
 }
