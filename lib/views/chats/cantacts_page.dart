db6c51122a943502274ebfb04ad4d68183400f51
Karlmathuthu <karlmathuthu11@gmail.com>
2023-11-08 13:40:12 +0200

    updated

--- lib/views/chats/cantacts_page.dart
+++ lib/views/chats/cantacts_page.dart
@@ -1,7 +1,10 @@
 import 'dart:io';
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:chat_mate_messanger/controllers/chat_controller.dart';
 import 'package:chat_mate_messanger/controllers/notifications_controller.dart';
+import 'package:chat_mate_messanger/utils/custom_icons.dart';
+import 'package:chat_mate_messanger/views/chats/chat_room_page.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
@@ -9,6 +12,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 import '../../theme/app_theme.dart';
 
@@ -32,7 +36,7 @@ class _ContactsPageState extends State<ContactsPage> {
     Get.dialog(
       CupertinoAlertDialog(
         title: Text(
-          "Wave at $mateName \n\n👋\n",
+          "Hi to $mateName \n\n👋\n",
           style: GoogleFonts.lato(
             fontWeight: FontWeight.normal,
           ),
@@ -63,7 +67,7 @@ class _ContactsPageState extends State<ContactsPage> {
             onPressed: () async {
               Get.back();
               //send a wave to mate
-              waveAtMate(currentUserUid, mateUid);
+              // waveAtMate(currentUserUid, mateUid);
               //send a notification.
               NotificationsController.sendMessageNotification(
                 userToken: mateToken,
@@ -80,17 +84,17 @@ class _ContactsPageState extends State<ContactsPage> {
     );
   }
 
-  void waveAtMate(String currentUserUid, String mateUid) {
-    chatController.sendAWaveToMate(
-      members: [
-        currentUserUid,
-        mateUid,
-      ],
-      senderId: currentUserUid,
-      messageText: "👋👋",
-      type: "wave",
-    );
-  }
+  // void waveAtMate(String currentUserUid, String mateUid) {
+  //   chatController.sendAWaveToMate(
+  //     members: [
+  //       currentUserUid,
+  //       mateUid,
+  //     ],
+  //     senderId: currentUserUid,
+  //     messageText: "👋👋",
+  //     type: "wave",
+  //   );
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -133,7 +137,7 @@ class _ContactsPageState extends State<ContactsPage> {
           const SizedBox(width: 10),
         ],
         title: Text(
-          "Find Mate",
+          "Find Mates",
           style: GoogleFonts.lato(
             color: Colors.black,
             fontSize: 18,
@@ -182,105 +186,159 @@ class _ContactsPageState extends State<ContactsPage> {
               ),
             ),
             const SizedBox(height: 10),
-            StreamBuilder(
-              stream: firestore
-                  .collection("users")
-                  .orderBy("userStatus", descending: true)
-                  .snapshots(),
-              builder: (context, snapshot) {
-                if (!snapshot.hasData) {
-                  return Center();
-                } else if (snapshot.connectionState ==
-                    ConnectionState.waiting) {
-                  return Center();
-                } else {
-                  return ListView.builder(
-                    itemCount: snapshot.data!.docs.length,
-                    shrinkWrap: true,
-                    physics: const ClampingScrollPhysics(),
-                    itemBuilder: (context, index) {
-                      bool isUserOnline =
-                          snapshot.data!.docs[index]["userStatus"] == "online";
+            Expanded(
+              child: StreamBuilder(
+                stream: firestore
+                    .collection("users")
+                    .orderBy("userStatus", descending: true)
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return Center();
+                  } else if (snapshot.connectionState ==
+                      ConnectionState.waiting) {
+                    return Center(
+                      child: LoadingAnimationWidget.fourRotatingDots(
+                        color: AppTheme.loaderColor,
+                        size: 40,
+                      ),
+                    );
+                  } else {
+                    return ListView.builder(
+                      itemCount: snapshot.data!.docs.length,
+                      shrinkWrap: true,
+                      physics: const ClampingScrollPhysics(),
+                      itemBuilder: (context, index) {
+                        bool isUserOnline = snapshot.data!.docs[index]
+                                ["userStatus"] ==
+                            "online";
 
-                      String username = snapshot.data!.docs[index]["userName"];
-                      String mateUid = snapshot.data!.docs[index]["userUid"];
-                      String mateToken = snapshot.data!.docs[index]["fcmToken"];
-                      String currentUserUid =
-                          FirebaseAuth.instance.currentUser!.uid;
-                      String initials = username[0].toUpperCase() +
-                          username[username.length - 1].toUpperCase();
+                        String username =
+                            snapshot.data!.docs[index]["userName"];
+                        String mateUid = snapshot.data!.docs[index]["userUid"];
+                        String mateToken =
+                            snapshot.data!.docs[index]["fcmToken"];
+                        String currentUserUid =
+                            FirebaseAuth.instance.currentUser!.uid;
+                        bool hasProfilePicture =
+                            snapshot.data!.docs[index]["photoUrl"] != "none";
+                        bool isVerified =
+                            snapshot.data!.docs[index]["isVerified"];
+                        String initials = username[0].toUpperCase() +
+                            username[username.length - 1].toUpperCase();
+                        if (mateUid == currentUserUid) {
+                          return const SizedBox();
+                        }
 
-                      return ListTile(
-                        onTap: () async {
-                          if (mateUid == currentUserUid) {
-                            Get.snackbar("No no 😊😳",
-                                "You can't send a message to yourself Mate!");
-                          } else {
-                            showSendWaveDialog(
-                              username,
-                              mateUid,
-                              mateToken,
-                            );
-                          }
-                        },
-                        title: Text(
-                          "@${snapshot.data!.docs[index]["userName"]}",
-                          style: GoogleFonts.lato(
-                            color: Colors.black,
-                            fontSize: 14,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                        subtitle: Text(
-                          snapshot.data!.docs[index]["userBio"],
-                          style: GoogleFonts.lato(
-                            color: Colors.black54,
-                            fontSize: 12,
-                            fontWeight: FontWeight.normal,
-                          ),
-                        ),
-                        leading: Container(
-                          height: 50,
-                          width: 50,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(25),
-                            color: AppTheme.loaderColor,
-                          ),
-                          child: Stack(
-                            children: [
-                              Center(
-                                child: Text(
-                                  initials,
-                                  style: GoogleFonts.lato(
-                                    fontSize: 16,
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ),
-                              if (isUserOnline) ...{
-                                Positioned(
-                                  bottom: 0,
-                                  right: 0,
-                                  child: Container(
-                                    width: 15,
-                                    height: 15,
-                                    decoration: const BoxDecoration(
-                                      color: Color.fromARGB(255, 73, 255, 167),
-                                      shape: BoxShape.circle,
+                        return ListTile(
+                          onTap: () async {
+                            if (mateUid == currentUserUid) {
+                              Get.snackbar("No no 😳",
+                                  "You can't send a message to yourself Mate!");
+                            } else if (username == "chatmate") {
+                              Get.dialog(
+                                CupertinoAlertDialog(
+                                  title: Text(
+                                    "Note Mate!",
+                                    style: GoogleFonts.lato(
+                                      fontWeight: FontWeight.normal,
+                                      color: Colors.red,
                                     ),
                                   ),
-                                )
-                              } else
-                                const SizedBox()
+                                  content: Text(
+                                    "You can't send a wave at the chatMate's official account!",
+                                    style: GoogleFonts.lato(
+                                        color: CupertinoColors.black),
+                                  ),
+                                  actions: <Widget>[
+                                    CupertinoDialogAction(
+                                      child: Text(
+                                        "Understood!",
+                                        style: GoogleFonts.lato(
+                                          color: AppTheme.mainColor,
+                                        ),
+                                      ),
+                                      onPressed: () {
+                                        Get.back();
+                                      },
+                                    ),
+                                  ],
+                                ),
+                              );
+                            } else {
+                              showSendWaveDialog(username, mateUid, mateToken);
+                            }
+                          },
+                          title: Row(
+                            children: [
+                              Text(
+                                "@${snapshot.data!.docs[index]["userName"]}",
+                                style: GoogleFonts.lato(
+                                  color: Colors.black,
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                              const SizedBox(width: 3),
+                              isVerified
+                                  ? const Icon(
+                                      Icons.verified,
+                                      color: AppTheme.mainColor,
+                                      size: 16,
+                                    )
+                                  : const SizedBox(),
                             ],
                           ),
-                        ),
-                      );
-                    },
-                  );
-                }
-              },
+                          subtitle: Text(
+                            snapshot.data!.docs[index]["userBio"],
+                            style: GoogleFonts.lato(
+                              color: Colors.black54,
+                              fontSize: 12,
+                              fontWeight: FontWeight.normal,
+                            ),
+                          ),
+                          leading: Container(
+                            height: 50,
+                            width: 50,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(25),
+                              color: AppTheme.loaderColor,
+                            ),
+                            child: Stack(
+                              children: [
+                                hasProfilePicture
+                                    ? CachedNetworkImage(
+                                        imageUrl: snapshot.data!.docs[index]
+                                            ["photoUrl"],
+                                      )
+                                    : Center(
+                                        child: SvgPicture.asset(
+                                            CustomIcons.defaultIcon),
+                                      ),
+                                if (isUserOnline) ...{
+                                  Positioned(
+                                    bottom: 0,
+                                    right: 0,
+                                    child: Container(
+                                      width: 15,
+                                      height: 15,
+                                      decoration: const BoxDecoration(
+                                        color: AppTheme.onlineStatus,
+                                        shape: BoxShape.circle,
+                                      ),
+                                    ),
+                                  )
+                                } else
+                                  const SizedBox()
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  }
+                },
+              ),
             ),
           ],
         ),
